4d647d4523c02c861176dbf482a42c4a937424d8
zwanga <Zwanga>
2020-04-19 12:33:14 +0200

    GUI Update

--- GUI.java
+++ GUI.java
@@ -9,7 +9,18 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JPanel;
 
-public class GUI{
+public class GUI {
+	static JTextField searchArea = new JTextField(7);
+	static JTextField searchArea1 = new JTextField(7);
+	static JTextField searchArea2 = new JTextField(7);
+
+	public static class ButtonListener implements ActionListener{
+		 public void actionPerformed(ActionEvent e) {
+			searchArea.setText("hello");
+    			}
+
+	}
+	
 	public static void main(String[] args){
 	JFrame frame = new JFrame("LoadShed App");
   
@@ -20,11 +31,16 @@ public class GUI{
 	labelResults.setBackground(Color.ORANGE);
 	labelResults.setFont(new Font("Tahoma", Font.ITALIC, 20));
 
+	JLabel label1 = new JLabel("Date");
+	JLabel label2 = new JLabel("Area");
+	JLabel label3 = new JLabel("Zone");
 
         JLabel line = new JLabel("--------------------------------------------------");
 	line.setForeground(Color.RED);
 
-        JTextField searchArea = new JTextField(30);
+        
+		
+	
         JTextField textDescription = new JTextField(20);
 
 	final Color VERY_LIGHT_RED = new Color(255,102,102);
@@ -33,6 +49,8 @@ public class GUI{
 
 	// Creating a button
         JButton searchButton = new JButton("SEARCH");
+	searchButton.addActionListener(new ButtonListener());
+
 	searchButton.setBackground(Color.ORANGE);
     	searchButton.setForeground(Color.WHITE);
 	searchButton.setBorder(BorderFactory.createEtchedBorder());
@@ -51,9 +69,30 @@ public class GUI{
 	constraints.gridy = 0;
 	constraints.gridwidth = 2;
         newPanel.add(searchArea, constraints);
+	
+	constraints.gridx = 2;
+	newPanel.add(label1,constraints);
+
+	constraints.gridx = 0;
+	constraints.gridy = 1;
+	constraints.gridwidth = 2;
+        newPanel.add(searchArea1, constraints);
+
+	constraints.gridx = 2;
+	newPanel.add(label2,constraints);
+
+	constraints.gridx = 0;
+	constraints.gridy = 2;
+	constraints.gridwidth = 2;
+        newPanel.add(searchArea2, constraints);
+
+
+	constraints.gridx = 2;
+	newPanel.add(label3,constraints);
+
 
         constraints.gridx = 0;
-        constraints.gridy = 1;
+        constraints.gridy = 4;
 	constraints.gridwidth = 2;
         constraints.anchor = GridBagConstraints.CENTER;     
         newPanel.add(searchButton, constraints);
@@ -65,11 +104,10 @@ public class GUI{
         newPanel.add(line, constraints);	
 
 	constraints.gridx = 0;
-	constraints.gridy = 4;
+	constraints.gridy = 5;
 	constraints.gridwidth = 2;
         newPanel.add(labelResults, constraints);
 
-	
 
 	
 	
